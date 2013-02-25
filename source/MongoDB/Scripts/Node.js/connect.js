a73c6324e550ee23a9e64a89a2c12b88f5025c67
Luis Ibanez <luis.ibanez@kitware.com>
2013-02-24 20:33:56 -0500

    Split MongoDB scripts: Javascript vs Node.js.

---
+++ source/MongoDB/Scripts/Node.js/connect.js
@@ -0,0 +1,9 @@
+// Retrieve
+var MongoClient = require('mongodb').MongoClient;
+
+// Connect to the db
+MongoClient.connect("mongodb://localhost:27017/exampleDb", function(err, db) {
+  if(!err) {
+    console.log("We are connected");
+  }
+});
