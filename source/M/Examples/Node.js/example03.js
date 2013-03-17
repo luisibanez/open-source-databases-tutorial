b0e1e9473721457b9cc95023ce74e5ae647f3450
Luis Ibanez <luis.ibanez@kitware.com>
2013-03-17 19:55:17 -0400

    Example of Get

---
+++ source/M/Examples/Node.js/example03.js
@@ -0,0 +1,16 @@
+var nodem = require('nodem');
+
+var db = new nodem.Gtm();
+
+console.log("Getting a Global");
+
+db.open();
+
+node = {global: 'myGlobal', subscripts: ["testing"]};
+
+var data = db.get(node).data;
+
+console.log( data );
+
+db.close();
+
