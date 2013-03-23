319e9bdf4d333e826b7b847332cd1adae774c80d
Luis Ibanez <luis.ibanez@kitware.com>
2013-03-23 18:51:50 -0400

    First two examples using ewdGlobals.
    Creating a basic global, from example in
    http://robtweed.wordpress.com/2013/01/24/a-phoenix-rises/

---
+++ source/M/Examples/Node.js/example10.js
@@ -0,0 +1,29 @@
+var ewd = require('ewdgateway2');
+var nodem = require('nodem');
+
+var db = new nodem.Gtm();
+
+ewd.init(db);
+
+console.log("Setting a Global");
+
+db.open();
+
+var patient = new ewd.GlobalNode('patient',[123456]);
+
+patient._delete();
+
+var document = {
+  "name": "John Doe",
+  "city": "New York"
+  };
+
+patient._setDocument(document);
+
+patient._fixProperties();
+
+console.log("Name: " + patient.name._value);
+console.log("City: " + patient.city._value);
+
+db.close();
+
---
+++ source/M/Examples/Node.js/example11.js
@@ -0,0 +1,29 @@
+var ewd = require('ewdgateway2');
+var nodem = require('nodem');
+
+var db = new nodem.Gtm();
+
+ewd.init(db);
+
+console.log("Setting a Global");
+
+db.open();
+
+var patient = new ewd.GlobalNode('patient',[123456]);
+
+patient._delete();
+
+var document = {
+  "name": "John Doe",
+  "city": "New York"
+  };
+
+patient._setDocument(document);
+
+patient._fixProperties();
+
+var record = patient._getDocument();
+console.log("patient info: " + JSON.stringify(record));
+
+db.close();
+
